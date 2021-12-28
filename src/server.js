ab15770616ced0453727ea710884cc6239aefcb9
곽세진_2004_anl <Reblackraven@Gmail.com>
2021-12-29 00:16:02 +0900

    Feat: connect front to back using socketIO

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
 import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 import dotenv from "dotenv";
 
@@ -15,34 +16,40 @@ app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app); // node js
+const httpServer = http.createServer(app); // node js
+const wsServer = SocketIO(httpServer);
 
-const wss = new WebSocket.Server({ server }); // 이렇게 하면 같은 서버에서 둘다 돌릴 수 있음
-// http 서버 위에 ws를 올림
-
-const socketsDb = [];
-
-wss.on("connection", (socket) => {
-  socketsDb.push(socket);
-  socket["nickname"] = "Anon";
-  // front에서 전달한 socket을 파라미터로 받음
-  console.log("Connected to Browser ✅");
-  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        socketsDb.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname} : ${message.payload}`)
-        );
-        break; // break 사용 안하면 닉네임이 바뀜!
-      case "nickname":
-        socket["nickname"] = message.payload;
-        break;
-    }
-  });
-
-  socket.send("hello"); // 연결이 끈어 졌을 때 보내진다!!
+wsServer.on("connection", (socket) => {
+  console.log(socket);
 });
 
-server.listen(process.env.PORT, handleListen);
+// websocket
+// const wss = new WebSocket.Server({ server }); // 이렇게 하면 같은 서버에서 둘다 돌릴 수 있음
+// // http 서버 위에 ws를 올림
+
+// const socketsDb = [];
+
+// wss.on("connection", (socket) => {
+//   socketsDb.push(socket);
+//   socket["nickname"] = "Anon";
+//   // front에서 전달한 socket을 파라미터로 받음
+//   console.log("Connected to Browser ✅");
+//   socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         socketsDb.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname} : ${message.payload}`)
+//         );
+//         break; // break 사용 안하면 닉네임이 바뀜!
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//         break;
+//     }
+//   });
+
+//   socket.send("hello"); // 연결이 끈어 졌을 때 보내진다!!
+// });
+
+httpServer.listen(process.env.PORT, handleListen);
